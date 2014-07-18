df8bcc13104ad095ad97c88e45d583837172cced
Cecile Veneziani <zora@patapouf.org>
2014-07-18 11:00:04 +0200

    Add ruby advanced course

--- config.rb
+++ config.rb
@@ -3,3 +3,8 @@ activate :bower
 set :css_dir,     'stylesheets'
 set :images_dir,  'images'
 set :js_dir,      'javascripts'
+
+%w(ruby-advanced).each do |name|
+  proxy "/courses/#{name}.html",        "/courses/#{name}.tmpl.html", locals: { media_type: 'screen' }
+  proxy "/courses/print/#{name}.html",  "/courses/#{name}.tmpl.html", locals: { media_type: 'print' }
+end
