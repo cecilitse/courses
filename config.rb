62c17a22bdc1691e03d96bf8a1c0241656b89aa3
Cecile Veneziani <zora@patapouf.org>
2014-08-29 12:53:29 +0200

    Refactor programs routes in config.rb

--- config.rb
+++ config.rb
@@ -4,11 +4,7 @@ set :css_dir,     'stylesheets'
 set :images_dir,  'images'
 set :js_dir,      'javascripts'
 
-template_extension  = '.tmpl.html.slim'
-program_filenames   = Dir[File.join(File.dirname(__FILE__), "source/programs/*#{template_extension}")]
-program_names       = program_filenames.map { |filename| File.basename(filename, template_extension) }
-
-program_names.each do |name|
-  proxy "/programs/#{name}.html",        "/programs/#{name}.tmpl.html"
-  proxy "/programs/print/#{name}.html",  "/programs/#{name}.tmpl.html"
+data.courses.each do |_code, course|
+  proxy "/programs/#{course.link}.html",        "/programs/#{course.link}.tmpl.html"
+  proxy "/programs/print/#{course.link}.html",  "/programs/#{course.link}.tmpl.html"
 end
