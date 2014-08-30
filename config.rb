f132ea267db4e5038d10a1e1925298988ecb1e7d
Cecile Veneziani <zora@patapouf.org>
2014-08-30 12:00:36 +0200

    Ignore template files in build

--- config.rb
+++ config.rb
@@ -4,6 +4,8 @@ set :css_dir,     'stylesheets'
 set :images_dir,  'images'
 set :js_dir,      'javascripts'
 
+ignore '*.tmpl.html.slim'
+
 data.courses.each do |_code, course|
   proxy "/programs/#{course.link}.html",        "/programs/#{course.link}.tmpl.html"
   proxy "/programs/print/#{course.link}.html",  "/programs/#{course.link}.tmpl.html"
