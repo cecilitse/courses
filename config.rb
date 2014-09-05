5e1feb02d25fff358417382c5bff5dd514d0583e
Cecile Veneziani <zora@patapouf.org>
2014-09-05 20:30:17 +0200

    Disable compass debug and line comments on build

--- config.rb
+++ config.rb
@@ -6,6 +6,13 @@ set :js_dir,      'javascripts'
 
 ignore '*.tmpl.html.slim'
 
+configure :build do
+  compass_config do |config|
+    config.sass_options = { debug_info:     false }
+    config.sass_options = { line_comments:  false }
+  end
+end
+
 data.courses.each do |_code, course|
   proxy "/programs/#{course.link}.html",        "/programs/#{course.link}.tmpl.html"
   proxy "/programs/print/#{course.link}.html",  "/programs/#{course.link}.tmpl.html"
