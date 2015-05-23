7b7915c2083d081c9eb83cc11863c4a6014bc34e
David Leuliette <dleuliette@gmail.com>
2015-05-24 00:58:07 +0200

    Add credentials in settings

--- config.rb
+++ config.rb
@@ -1,8 +1,9 @@
 activate :bower
+activate :livereload
 
-set :css_dir,     'stylesheets'
-set :images_dir,  'images'
-set :js_dir,      'javascripts'
+set :js_dir,     'assets/javascripts'
+set :css_dir,    'assets/stylesheets'
+set :images_dir, 'assets/images'
 
 ignore '*.tmpl.html.slim'
 
@@ -18,6 +19,11 @@ configure :build do
     config.sass_options = { debug_info:     false }
     config.sass_options = { line_comments:  false }
   end
+
+  activate :minify_html, remove_input_attributes: false
+  activate :minify_css
+  activate :minify_javascript
+  activate :gzip
 end
 
 data.courses.each do |_code, course|
