155e1501c78d8fa98cb6ad2fffacaa6608bc2d00
David Leuliette <dleuliette@gmail.com>
2017-04-06 00:49:37 +0200

    asset pipeline talk

--- Gemfile
+++ Gemfile
@@ -1,14 +1,19 @@
 source 'https://rubygems.org'
-gem 'therubyracer'
 
+# Core
 gem 'middleman',                 '~> 3.3.12'
 gem 'middleman-autoprefixer',    '~> 2.5.0'
 gem 'middleman-bower',           '~> 1.0.2'
 gem 'middleman-deploy',          '~> 1.0.0'
+gem 'middleman-favicon-maker',   '~>  3.7'
 gem 'middleman-google-analytics'
 gem 'middleman-livereload',      '~> 3.4.2'
 gem 'middleman-minify-html',     '~> 3.4.1'
 
+# Linux Support
+gem 'therubyracer'
+
+# Templating Engines
 gem 'builder',  '~> 3.2.2'
 gem 'slim',     '~> 3.0.2'
 gem 'thor',     '~> 0.19.1'
--- config.rb
+++ config.rb
@@ -22,12 +22,28 @@ activate :deploy do |deploy|
 end
 
 configure :build do
+  activate :favicon_maker do |f|
+    f.template_dir  = File.join(root, 'source/assets/images/')
+    f.output_dir    = File.join(root, 'build')
+    f.icons = {
+      'favicon_base.png' => [
+        { icon: 'chrome-touch-icon-192x192.png' },
+        { icon: 'apple-touch-icon.png', size: '152x152' },
+        { icon: 'ms-touch-icon-144x144-precomposed.png', size: '144x144' },
+        { icon: 'favicon-196x196.png' },
+        { icon: 'favicon-160x160.png' },
+        { icon: 'favicon-96x96.png' },
+        { icon: 'favicon-32x32.png' },
+        { icon: 'favicon-16x16.png' },
+        { icon: 'favicon.ico', size: '64x64,32x32,24x24,16x16' },
+      ]
+    }
+  end
 
   activate :gzip
   activate :minify_css
   activate :minify_html, remove_input_attributes: false
   activate :minify_javascript
-
 end
 
 data.courses.each do |_code, course|
